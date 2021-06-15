f34a60a36eb8e5eeea4d742a47440af25c01b9ea
Nikki Mallory <grandmasternik>
2021-06-15 16:41:08 -0500

    wip: initial files added to models and views dir

---
+++ models/enhancements.js
@@ -0,0 +1,21 @@
+const Schema = mongoose.Schema;
+
+const enhancementSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    benefits: {
+        type: String,
+        required: true,
+    },
+    img: String,
+});
+
+// const Products = mongoose.model(`Products`, ProductsSchema)
+
+module.exports = mongoose.model(`Enhancement`, enhancementSchema);
---
+++ models/oils.js
@@ -0,0 +1,21 @@
+const Schema = mongoose.Schema;
+
+const oilSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    benefits: {
+        type: String,
+        required: true,
+    },
+    img: String,
+});
+
+// const Products = mongoose.model(`Products`, ProductsSchema)
+
+module.exports = mongoose.model(`Oil`, oilSchema);
