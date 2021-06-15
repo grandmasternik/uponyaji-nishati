3129de7ca6feae3b4290667017b5569e43f0e199
Nikki Mallory <grandmasternik>
2021-06-15 12:53:36 -0500

    mongoose config/server connected

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// Configure Mongoose
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+db.on('connected', () => console.log('mongo connected'));
+db.on('error', (err) => console.log(err.message, ' is mongo connected?'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
 
 // Listener
 const PORT = process.env.PORT;
